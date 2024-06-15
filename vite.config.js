cc58fe0aa610a7873dd41818d258efabd97866a6
Max Carrión <141116497+MaxCar31@users.noreply.github.com>
2024-06-14 21:57:25 -0500

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/BrailleTech_WebSite/', 
+  base: '/BrailleTech_WebSite/',  // Asegúrate de que coincide con el nombre de tu repositorio
   root: 'src',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true,
   },
   server: {
-    open: true
-  }
+    open: true,
+  },
 });
